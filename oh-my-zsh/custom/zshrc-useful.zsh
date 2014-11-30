e69a01118829f0fcbbab335b13aa8c764c073feb
machupicchubeta <machupicchubeta@gmail.com>
2014-11-30 17:15:39 +0900

    Manage aliases :metal:

--- oh-my-zsh/custom/zshrc-useful.zsh
+++ oh-my-zsh/custom/zshrc-useful.zsh
@@ -47,3 +47,19 @@ setopt extended_glob
 
 # keybind
 bindkey '^R' history-incremental-pattern-search-backward
+
+alias -g L='| less'
+alias -g G='| grep'
+
+# mollifier delta blog : http://mollifier.hatenablog.com/entry/20100317/p1
+if which pbcopy >/dev/null 2>&1 ; then
+    # Mac
+    alias -g C='| pbcopy'
+elif which xsel >/dev/null 2>&1 ; then
+    # Linux
+    alias -g C='| xsel --input --clipboard'
+elif which putclip >/dev/null 2>&1 ; then
+    # Cygwin
+    alias -g C='| putclip'
+fi
+
