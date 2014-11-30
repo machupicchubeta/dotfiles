cc6e75d8d8b3affcdc2c0b1e8c57368e133e5e82
machupicchubeta <machupicchubeta@gmail.com>
2014-11-30 16:43:26 +0900

    Customized zshrc :metal:

---
+++ oh-my-zsh/custom/zshrc-useful.zsh
@@ -0,0 +1,49 @@
+autoload -Uz colors
+colors
+
+bindkey -e
+
+export HISTFILE=~/.zsh_history
+
+autoload -Uz select-word-style
+select-word-style default
+
+zstyle ':zle:*' word-chars " /=;@:{},|"
+zstyle ':zle:*' word-style unspecified
+
+autoload -Uz compinit
+compinit
+
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+zstyle ':completion:*' ignore-parents parent pwd ..
+
+zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/X11R6/bin
+
+zstyle ':completion:*:processes' command 'ps x -o pid,s,args'
+
+setopt print_eight_bit
+
+setopt no_beep
+
+setopt no_flow_control
+
+setopt interactive_comments
+
+setopt auto_cd
+
+setopt auto_pushd
+setopt pushd_ignore_dups
+
+setopt share_history
+
+setopt hist_ignore_all_dups
+
+setopt hist_ignore_space
+
+setopt hist_reduce_blanks
+
+setopt extended_glob
+
+# keybind
+bindkey '^R' history-incremental-pattern-search-backward
