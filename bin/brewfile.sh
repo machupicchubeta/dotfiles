cdfe7b8623348be95db0e1f8775713bab5837099
machupicchubeta <machupicchubeta@gmail.com>
2015-10-14 00:32:06 +0900

    Ownership to me :beer: :metal:

--- bin/brewfile.sh
+++ bin/brewfile.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo chown -R $(whoami):admin /usr/local/
 
 brew update
 brew upgrade
--- bin/convert_brewfile_to_shell_script.rb
+++ bin/convert_brewfile_to_shell_script.rb
@@ -1,6 +1,8 @@
 #!/usr/local/bin/ruby
 File::open( ARGV[0] ) {|brewfile|
-  print "#!/bin/sh"
+  puts "#!/bin/sh"
+  puts 'sudo chown -R $(whoami):admin /usr/local/'
+
   brewfile.each_line {|line|
     if line[0] == "#"|| line.size == 1 then
       print line
