c286845182471f04da00b14b7b2ec4fd326ffaa2
machupicchubeta <machupicchubeta@gmail.com>
2016-08-28 13:18:02 +0900

    Let remove outdated casks :metal:

--- bin/convert_brewfile_to_shell_script.rb
+++ bin/convert_brewfile_to_shell_script.rb
@@ -8,7 +8,7 @@ File.open ARGV[0] do |brewfile|
     elsif line.start_with?('cask install')
       app_name = line.chomp.gsub('cask install', '').strip
 
-      puts "if [[ $(brew cask info #{app_name} | grep 'Not installed') ]]; then if [[ $(ls /usr/local/Caskroom | grep #{app_name}) ]]; then brew cask uninstall #{app_name} --force && brew #{line.chomp}; else brew #{line.chomp}; fi; else echo '#{app_name} is currently the newest version available'; fi"
+      puts "if [[ $(brew cask info #{app_name} | grep 'Not installed') || $(brew cask info #{app_name} | grep '/usr/local/Caskroom/#{app_name}' | wc -l) > 1 ]]; then if [[ $(ls /usr/local/Caskroom | grep #{app_name}) ]]; then brew cask uninstall #{app_name} --force && brew #{line.chomp}; else brew #{line.chomp}; fi; else echo '#{app_name} is currently the newest version available'; fi"
     else
       print "brew #{line}"
     end
