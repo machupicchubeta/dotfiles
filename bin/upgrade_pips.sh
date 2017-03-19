02dfea6cb7b4b48d216ccc3d5f5bebc11284b021
machupicchubeta <machupicchubeta@gmail.com>
2017-03-19 17:23:50 +0900

    Use the upgrade script of each python version :dragon: :metal:

--- bin/upgrade_pips.sh
+++ bin/upgrade_pips.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # pip
-pip install --upgrade pip setuptools
+pip install --upgrade pip setuptools wheel
 pip list --outdated | awk '{print $1}' | xargs pip install -U
 pip install neovim
+
+# pip3
+pip3 install --upgrade pip setuptools wheel
+pip3 list --outdated | awk '{print $1}' | xargs pip install -U
+pip3 install neovim
