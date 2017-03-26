c261046f556d874e74ca4b286b6ffc4fad31f2ff
machupicchubeta <machupicchubeta@gmail.com>
2017-03-26 20:28:35 +0900

    Remove redundant script in python3 environment :put_litter_in_its_place: :metal:

--- bin/upgrade_pips.sh
+++ bin/upgrade_pips.sh
@@ -4,8 +4,3 @@
 pip install --upgrade pip setuptools wheel
 pip list --outdated | awk '{print $1}' | xargs pip install -U
 pip install neovim
-
-# pip3
-pip3 install --upgrade pip setuptools wheel
-pip3 list --outdated | awk '{print $1}' | xargs pip install -U
-pip3 install neovim
