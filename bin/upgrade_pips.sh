f45bba4b2af2acd681901684bccd83519069bcbc
machupicchubeta <machupicchubeta@gmail.com>
2017-11-19 03:40:04 +0900

    Ensure upgrade via pip3
    Because this environment has both python versions 2 and 3. :sake: :sushi:

--- bin/upgrade_pips.sh
+++ bin/upgrade_pips.sh
@@ -3,7 +3,7 @@
 # pip2
 pip2 install neovim
 
-# pip
-pip install --upgrade pip setuptools wheel
-pip list --outdated | awk '{print $1}' | xargs pip install -U
-pip install neovim
+# pip3
+pip3 install --upgrade pip setuptools wheel
+pip3 list --outdated | awk '{print $1}' | xargs pip3 install -U
+pip3 install neovim
