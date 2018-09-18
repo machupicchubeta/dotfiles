78c6b2f2ec0bc0d6471ac22cc0552a9cde1dd675
machupicchubeta <machupicchubeta@gmail.com>
2018-09-18 12:14:55 +0900

    Releasing the full power of `neovim`
    It seems necessary to install "python provider" for `neovim` also in `pip3` installed by homebrew.

--- bin/upgrade_pips.sh
+++ bin/upgrade_pips.sh
@@ -10,3 +10,4 @@ pip2 install neovim
 pip3 install --upgrade pip setuptools wheel
 pip3 list --outdated | awk '{print $1}' | xargs pip3 install -U
 pip3 install neovim
+/usr/local/bin/pip3 install neovim
