197f3061b9b8e5824e65f4b06418823031b486fe
machupicchubeta <machupicchubeta@gmail.com>
2018-12-09 23:14:33 +0900

    Make `zsh` completions settings more fruitful
    Refs:
    - https://github.com/zsh-users/zsh-completions/blob/master/zsh-completions-howto.org
    - http://zsh.sourceforge.net/Doc/Release/Completion-System.html#Completion-Functions
    - http://zsh.sourceforge.net/Doc/Release/Completion-System.html#Control-Functions
    
    and Articles that I referred to:
    - https://qiita.com/kotashiratsuka/items/ae37757aa1d31d1d4f4d
    - http://voidy21.hatenablog.jp/entry/20090902/1251918174
    - http://www.gentei.org/~yuuji/rec/pc/zsh/zshcompsys.txt
    - https://qiita.com/tani-shi/items/fd0105d562edb235deca
    - https://gihyo.jp/dev/serial/01/zsh-book/0005
    - https://rcmdnk.com/blog/2015/05/15/computer-linux-mac-zsh/

--- oh-my-zsh/custom/zshrc-useful.zsh
+++ oh-my-zsh/custom/zshrc-useful.zsh
@@ -14,38 +14,55 @@ zstyle ':zle:*' word-style unspecified
 autoload -Uz compinit
 compinit
 
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
+autoload predict-on
+zle -N predict-on
+zle -N predict-off
+bindkey '' predict-on
+bindkey '' predict-off
+zstyle ':predict' verbose true
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z} r:|[-_.]=**'
+zstyle ':completion:*' completer _complete _ignored _cmdstring _canonical_paths _expand _extensions _external_pwds _expand_alias _files _multi_parts
+zstyle ':completion:*:cd:*' tag-order local-directories path-directories
 zstyle ':completion:*' ignore-parents parent pwd ..
-
 zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/X11R6/bin
-
 zstyle ':completion:*:processes' command 'ps x -o pid,s,args'
 
 setopt print_eight_bit
-
 setopt no_beep
-
 setopt no_flow_control
-
 setopt interactive_comments
-
 setopt auto_cd
+cdpath=(.)
 
+setopt auto_menu
+setopt auto_list
+setopt auto_name_dirs
+setopt auto_param_keys
+setopt cdable_vars
+setopt mark_dirs
+setopt auto_param_slash
+setopt list_packed
+setopt numeric_glob_sort
+setopt magic_equal_subst
+setopt brace_ccl
+setopt append_history
+setopt hist_expire_dups_first
+setopt hist_find_no_dups
+setopt hist_no_functions
+setopt hist_no_store
+setopt hist_reduce_blanks
+setopt hist_save_no_dups
+setopt hist_expand
+setopt function_argzero
+setopt notify
 setopt auto_pushd
 setopt pushd_ignore_dups
-
 setopt share_history
-
 setopt hist_ignore_all_dups
-
 setopt hist_ignore_space
-
 setopt hist_reduce_blanks
-
 setopt extended_glob
 
-# keybind
 bindkey '^[R' history-incremental-pattern-search-backward
 bindkey '^[S' history-incremental-pattern-search-forward
 bindkey "^[u" undo
@@ -62,4 +79,3 @@ elif which putclip >/dev/null 2>&1 ; then
     # Cygwin
     alias -g C='| putclip'
 fi
-
