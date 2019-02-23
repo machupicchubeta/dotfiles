eec2456ee76345fdef813ec2ceef0301d94943fd
machupicchubeta <machupicchubeta@gmail.com>
2019-02-23 22:34:34 +0900

    The Python package `neovim` was renamed to `pynvim`
    See also:
    - https://github.com/neovim/neovim/wiki/Following-HEAD#20181118

--- bin/upgrade_pips.sh
+++ bin/upgrade_pips.sh
@@ -4,12 +4,12 @@
 pip install --upgrade pip
 
 # pip2
-pip2 install neovim
+pip2 install pynvim
 
 # pip3
 pip3 install --upgrade pip setuptools wheel
 pip3 list --outdated | awk 'NR>2{print $1}' | xargs pip3 install -U
 pip3 install awscli
 pip3 install aws-shell
-pip3 install neovim
-/usr/local/bin/pip3 install neovim
+pip3 install pynvim
+/usr/local/bin/pip3 install pynvim
