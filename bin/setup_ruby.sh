93e1b1dacc6b82f666e79a29705500d1414b3e66
machupicchubeta <machupicchubeta@gmail.com>
2019-11-06 04:43:55 +0900

    Whitespaces gone
    ```
    $ rbenv install -l | grep -E '^(\d+\.\d+)\.\d+$' | tail -n 1
    2.6.5
    ```

--- bin/setup_ruby.sh
+++ bin/setup_ruby.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 
-latest_ruby_version=$(rbenv install -l | grep -E '^\s+(\d+\.\d+)\.\d+$' | tail -n 1)
+latest_ruby_version=$(rbenv install -l | grep -E '^(\d+\.\d+)\.\d+$' | tail -n 1)
 rbenv install --skip-existing $latest_ruby_version
 rbenv global $latest_ruby_version
 rbenv rehash
