fdd76a14ff51c647eca9bdfaadc4b8e9eaefd1bc
machupicchubeta <machupicchubeta@gmail.com>
2019-11-09 12:40:56 +0900

    Add `go` setup script
    Ref.
    - https://golang.org
    - https://github.com/syndbg/goenv
    - https://github.com/syndbg/goenv/blob/master/INSTALL.md#homebrew-on-mac-os-x
    - https://github.com/syndbg/goenv/blob/master/COMMANDS.md#goenv-init

---
+++ bin/setup_go.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+
+latest_go_version=$(goenv install -l | grep -E '^\s+(\d+\.\d+)\.\d+$' | tail -n 1 | sed -e 's/^[ ]*//g')
+goenv install --skip-existing $latest_go_version
+goenv global $latest_go_version
+goenv rehash
+
+eval "$(goenv init -)"
