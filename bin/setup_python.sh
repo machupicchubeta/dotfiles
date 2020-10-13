91ef39dbbd8195f43b067cd47c6846ea41e6c1a0
machupicchubeta <machupicchubeta@gmail.com>
2020-10-13 15:34:11 +0900

    Unify the notation of `upgrade` option

--- bin/setup_python.sh
+++ bin/setup_python.sh
@@ -13,7 +13,7 @@ pip install --upgrade --use-feature=2020-resolver pip
 
 # pip3
 pip3 install --upgrade --use-feature=2020-resolver pip setuptools wheel
-pip3 list --outdated | awk 'NR>2{print $1}' | xargs pip3 install -U --use-feature=2020-resolver
+pip3 list --outdated | awk 'NR>2{print $1}' | xargs pip3 install --upgrade --use-feature=2020-resolver
 pip3 install cfn-lint
 pip3 install pynvim
 /usr/local/bin/pip3 install pynvim
