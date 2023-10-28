5e2cbb667ef4f7b0e944142f32a7101b9253db8e
machupicchubeta <machupicchubeta@gmail.com>
2023-10-29 02:20:13 +0900

    Move Readline config file under XDG Base Directory
    See also:
    - https://wiki.archlinux.org/title/XDG_Base_Directory#Partial
    - https://wiki.archlinux.jp/index.php/XDG_Base_Directory#.E9.83.A8.E5.88.86.E7.9A.84.E3.81.AB.E3.82.B5.E3.83.9D.E3.83.BC.E3.83.88.E3.81.97.E3.81.A6.E3.81.84.E3.82.8B.E3.82.BD.E3.83.95.E3.83.88.E3.82.A6.E3.82.A7.E3.82.A2

--- bin/put_settings.sh
+++ bin/put_settings.sh
@@ -35,7 +35,7 @@ if [ -d "$SETTINGS_PATH/.vim" ]; then
   ln -s "$SETTINGS_PATH/.vim" "$XDG_CONFIG_HOME/nvim"
 fi
 
-for app in "git" "starship" "lsd" "sheldon" "gem" "rspec" "tmux"; do
+for app in "git" "starship" "lsd" "sheldon" "readline" "gem" "rspec" "tmux"; do
   if [ -L "$XDG_CONFIG_HOME/$app" ]; then
     unlink "$XDG_CONFIG_HOME/$app"
   fi
