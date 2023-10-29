e88ae89a54d51cd8c8e4200fe365ec4307c2f8ae
machupicchubeta <machupicchubeta@gmail.com>
2023-10-29 21:07:18 +0900

    Move Rustup completion scripts under XDG Base Directory
    Refs.
    - https://rust-lang.github.io/rustup/installation/index.html#enable-tab-completion-for-bash-fish-zsh-or-powershell

--- bin/setup_rust.sh
+++ bin/setup_rust.sh
@@ -2,6 +2,7 @@
 set -eu
 
 : "${XDG_DATA_HOME:=$HOME/.local/share}"
+: "${RUSTUP_HOME:=$XDG_DATA_HOME/rustup}"
 : "${CARGO_HOME:=$XDG_DATA_HOME/cargo}"
 
 # if [[ ! -d "$HOME/.cargo/bin" ]]; then RUSTUP_INIT_SKIP_PATH_CHECK=yes rustup-init -y --no-modify-path --default-host x86_64-apple-darwin --default-toolchain stable --profile default; fi
@@ -13,6 +14,6 @@ if [ "$(uname -m)" = "x86_64" ]; then
 elif [ "$(uname -m)" = "arm64" ]; then
   : "${HOMEBREW_PREFIX:=/opt/homebrew}"
 fi
-if [[ ! -f $HOMEBREW_PREFIX/etc/bash_completion.d/rustup.bash-completion ]]; then rustup completions bash > $HOMEBREW_PREFIX/etc/bash_completion.d/rustup.bash-completion; fi
-if [[ ! -d "$HOME/.zfunc" ]]; then mkdir -p "$HOME/.zfunc"; fi
-if [[ ! -f $HOME/.zfunc/_rustup ]]; then rustup completions zsh > $HOME/.zfunc/_rustup; fi
+if [[ ! -f "$HOMEBREW_PREFIX/etc/bash_completion.d/rustup.bash-completion" ]]; then rustup completions bash > "$HOMEBREW_PREFIX/etc/bash_completion.d/rustup.bash-completion"; fi
+if [[ ! -d "$RUSTUP_HOME" ]]; then mkdir -p "$RUSTUP_HOME"; fi
+if [[ ! -f "$RUSTUP_HOME/completions" ]]; then rustup completions zsh > "$RUSTUP_HOME/completions"; fi
