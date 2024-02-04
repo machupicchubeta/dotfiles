ad1e177c9fd5d9d9dfc60189570dbb8ff8c0dfc1
machupicchubeta <machupicchubeta@gmail.com>
2024-02-04 15:54:43 +0900

    Set LSP configs unique to the project I'm currently involved in

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -124,6 +124,12 @@ return {
               root_dir = lspconfig.util.root_pattern('package.json'),
               single_file_support = false
             }))
+          elseif server == 'goplus' then
+            lspconfig.gopls.setup(vim.tbl_extend('force', opt, {
+              env = {
+                GOFLAGS = '-tags=parallel,serial,integration'
+              }
+            }))
           else
             lspconfig[server].setup(opt)
           end
