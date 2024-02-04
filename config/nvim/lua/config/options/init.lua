a1fa1b7adaf24d9aa8da4503fd2de9958f52513c
machupicchubeta <machupicchubeta@gmail.com>
2024-02-04 15:48:25 +0900

    Set Neovim options only for the items I need now
    In addition, remove the settings that are the same as the initial values in the required items.

--- config/nvim/lua/config/options/init.lua
+++ config/nvim/lua/config/options/init.lua
@@ -1,7 +1,6 @@
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
-vim.opt.indentkeys = { '!^F', 'o', 'O', '0<Bar>', '0=where' }
 vim.opt.smarttab = true
 vim.opt.expandtab = true
 vim.opt.shiftround = true
@@ -12,76 +11,25 @@ vim.opt.clipboard:append({ 'unnamedplus' })
 vim.opt.backspace = { 'start', 'eol', 'indent' }
 vim.opt.fixeol = false
 vim.opt.showmatch = true
-vim.opt.cpoptions:remove({ 'm' })
 vim.opt.matchtime = 1
 vim.opt.matchpairs:append('<:>')
-vim.opt.hidden = true
-vim.opt.infercase = true
 vim.opt.grepprg = 'grep --ignore-case --line-number -H'
-vim.opt.isfname:remove({ '==' })
-vim.opt.timeout = true
-vim.opt.timeoutlen = 3000
-vim.opt.ttimeoutlen = 100
-vim.opt.updatetime = 1000
-vim.opt.virtualedit = 'block'
 vim.opt.keywordprg = ':help'
 vim.opt.backupcopy = 'yes'
-vim.opt.pastetoggle = '<C-E>'
 
 vim.opt.number = true
-vim.opt.list = true
-vim.opt.listchars = { tab = '>-', trail = '-', extends = '>', nbsp = '_', precedes = '<' }
-vim.opt.laststatus = 2
 vim.opt.cmdheight = 2
-vim.opt.showcmd = false
-vim.opt.showtabline = 0
-vim.opt.linebreak = false
-vim.opt.showbreak = '\\'
-vim.opt.breakat = ' ;:,!?\t'
-vim.opt.breakindent = true
-vim.opt.shortmess = 'aTIcF'
-vim.opt.showmode = false
-vim.opt.writebackup = false
-vim.opt.backup = false
 vim.opt.swapfile = false
-vim.opt.visualbell = false
-vim.opt.belloff = all
-vim.opt.errorbells = false
-vim.opt.wildmenu = false
-vim.opt.wildmode = { 'list:longest', 'full' }
-vim.opt.history = 1000
-vim.opt.showfulltag = true
-vim.opt.wildoptions = tagfile
 vim.opt.completeopt = 'menuone,noinsert,noselect'
-vim.opt.complete = '.,w,b,u,t,i'
-vim.opt.pumheight = 20
-vim.opt.report = 0
-vim.opt.startofline = false
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.winwidth = 30
-vim.opt.winheight = 1
-vim.opt.cmdwinheight = 5
-vim.opt.equalalways = false
-vim.opt.previewheight = 8
-vim.opt.helpheight = 12
-vim.opt.ttyfast = true
-vim.opt.display = lastline
-vim.opt.conceallevel = 2
-vim.opt.concealcursor = ''
 vim.opt.gdefault = true
 vim.opt.cursorline = true
-vim.opt.cursorcolumn = false
 vim.opt.mouse = 'a'
-vim.opt.ruler = true
-vim.opt.showmode = true
-vim.opt.scrolloff = 3
 vim.opt.helplang:append({ 'en', 'ja' })
 
-vim.opt.hlsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.incsearch = true
+
+vim.opt.formatoptions = 'q'
 
 vim.opt.termguicolors = true
 
