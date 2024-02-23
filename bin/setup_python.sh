b66515ed5d08ed08e0ce1a77672f93aca14049fb
machupicchubeta <machupicchubeta@gmail.com>
2024-02-24 02:33:30 +0900

    wip

--- bin/setup_python.sh
+++ bin/setup_python.sh
@@ -8,7 +8,13 @@ mise use --global python@latest
 mise prune python
 mise reshim
 
-python3 -m pip install --upgrade pip
-pip3 install --upgrade setuptools wheel
-pip3 list --outdated | awk 'NR>2{print $1}' | xargs --no-run-if-empty pip3 install --upgrade
-pip3 install pynvim
+which python3
+
+if command -v mise > /dev/null; then eval "$(mise activate zsh)"; fi
+
+which python3
+
+# python3 -m pip install --upgrade pip
+# pip3 install --upgrade setuptools wheel
+# pip3 list --outdated | awk 'NR>2{print $1}' | xargs --no-run-if-empty pip3 install --upgrade
+# pip3 install pynvim
