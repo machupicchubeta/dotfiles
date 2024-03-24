7866d46771089c96cbd13cbd3df76ab6d4ae3e10
machupicchubeta <machupicchubeta@gmail.com>
2024-03-24 22:29:47 +0900

    Fix according to ShellCheck SC2038 warning
    - https://www.shellcheck.net/wiki/SC2038
    
    > Use -print0/-0 or find -exec + to allow for non-alphanumeric filenames.

--- bin/setup_fonts.sh
+++ bin/setup_fonts.sh
@@ -8,13 +8,13 @@ echo -e "\nInstall \"Source Han Code JP\" to the user font directory"
 curl --fail --silent --show-error --location https://github.com/adobe-fonts/source-han-code-jp/archive/refs/tags/2.012R.tar.gz | tar -xzC /tmp/
 SOURCE_HAN_CODE_JP_TEMP_DIR="$TEMP_DIR/source-han-code-jp-2.012R"
 if [ -d "$SOURCE_HAN_CODE_JP_TEMP_DIR" ]; then
-  find "$USER_FONT_DIR" -type f -name "SourceHanCodeJP-*" | xargs rm --recursive --force
+  find "$USER_FONT_DIR" -type f -name "SourceHanCodeJP-*" -exec rm --recursive --force {} +
   find "$SOURCE_HAN_CODE_JP_TEMP_DIR" -type f \( -name '*.ttc' -or -name '*.otf' \) -exec mv --target-directory="$USER_FONT_DIR" {} +
   rm --recursive --force "$SOURCE_HAN_CODE_JP_TEMP_DIR"
 fi
 
 echo -e "\nInstall \"Moralerspace\" font families"
-find "$USER_FONT_DIR" -type f -name "Moralerspace*" | xargs rm --recursive --force
+find "$USER_FONT_DIR" -type f -name "Moralerspace*" -exec rm --recursive --force {} +
 MORALERSPACE_FAMILIES=(
   "Moralerspace"
   "MoralerspaceHW"
