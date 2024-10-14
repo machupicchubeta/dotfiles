97ddee32ae7996faf57d91a500890180d35c217f
machupicchubeta <machupicchubeta@gmail.com>
2024-10-14 16:33:09 +0900

    Simplify setup scripts by using the asdf-flutter patch version
    When set up on macOS using asdf-flutter, a zip file is downloaded.
    However, asdf-flutter does not support zip files and an error occurs.
    There is the Issue and the Pull Request in the repository, but it has not been merged for more than 2 years.
    Maybe there is some reason why some environments reproduce the issue and others do not.
    Or maybe the repository is not maintained.
    
    See also:
    - https://docs.flutter.dev/get-started/install/macos/mobile-ios#download-then-install-flutter
    - https://github.com/asdf-community/asdf-flutter/pull/52
    - https://github.com/asdf-community/asdf-flutter/issues/33
    - https://github.com/asdf-community/asdf-flutter/pull/36

--- bin/setup_flutter.sh
+++ bin/setup_flutter.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 set -eu
 
-if [ ! -x "$(command -v ghq)" ]; then
-  echo "Install ghq command first."
-  exit 1
-fi
-
-: "${REPOSITORIES_PATH:=$HOME/Repositories}"
-if [ ! -d "$REPOSITORIES_PATH" ]; then
-  mkdir "$REPOSITORIES_PATH"
-fi
-
-export GHQ_ROOT="${GHQ_ROOT:=$REPOSITORIES_PATH}"
-ghq get --update https://github.com/flutter/flutter
-
-export FLUTTER_ROOT="${FLUTTER_ROOT:=$REPOSITORIES_PATH/github.com/flutter/flutter}"
-git -C $FLUTTER_ROOT switch stable
-$FLUTTER_ROOT/bin/flutter channel stable
-
-unset REPOSITORIES_PATH
+# https://github.com/asdf-community/asdf-flutter/pull/52
+# https://github.com/asdf-community/asdf-flutter/issues/33
+# https://github.com/asdf-community/asdf-flutter/pull/36
+mise plugins install flutter https://github.com/hleinone/asdf-flutter
+mise plugins update flutter
+mise install flutter@latest
+mise upgrade flutter@latest
+mise use --global flutter@latest
+mise prune --yes flutter
+mise reshim
