6d2af0ee57b41cda4ff338b9e20c59abac9dec55
machupicchubeta <machupicchubeta@gmail.com>
2025-01-13 01:13:12 +0900

    Make it easy to read
    Separate definition and use of elements.

--- bin/put_settings.sh
+++ bin/put_settings.sh
@@ -30,7 +30,29 @@ if [ ! -d "$XDG_DATA_HOME" ]; then
   mkdir --parents "$XDG_DATA_HOME"
 fi
 
-for app in "git" "starship" "lsd" "sheldon" "bundle" "readline" "gem" "irb" "rspec" "tmux" "tig" "mise" "asdf" "direnv" "aws" "zsh-abbr" "nvim" "wezterm" "bat"; do
+APPS=(
+  "git"
+  "starship"
+  "lsd"
+  "sheldon"
+  "bundle"
+  "readline"
+  "gem"
+  "irb"
+  "rspec"
+  "tmux"
+  "tig"
+  "mise"
+  "asdf"
+  "direnv"
+  "aws"
+  "zsh-abbr"
+  "nvim"
+  "wezterm"
+  "bat"
+  )
+
+for app in "${APPS[@]}"; do
   if [ -L "$XDG_CONFIG_HOME/$app" ]; then
     unlink "$XDG_CONFIG_HOME/$app"
   fi
@@ -68,6 +90,7 @@ for app in "git" "starship" "lsd" "sheldon" "bundle" "readline" "gem" "irb" "rsp
   fi
 done
 unset -v app
+unset -v APPS
 unset -v shell
 unset -v arch
 
