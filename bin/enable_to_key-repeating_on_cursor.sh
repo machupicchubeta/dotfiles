ad87a790325ad4498e097e415abf91bcff8053d7
machupicchubeta <machupicchubeta@gmail.com>
2025-04-29 19:01:35 +0900

    Enable key-repeating on "Cursor"
    This script is similar to that of Visual Studio Code.
    
    See also:
    - https://github.com/machupicchubeta/dotfiles/commit/da93c7c8c63211e680148141638234d10619600b
    
    Cursor app id is:
    ```
    $ osascript -e 'id of app "Cursor"'
    
    com.todesktop.230313mzl4w4u92
    ```
    
    Refs.
    - https://forum.cursor.com/t/apple-press-and-hold-for-cursor/43134

---
+++ bin/enable_to_key-repeating_on_cursor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -eu
+
+defaults write com.todesktop.230313mzl4w4u92 ApplePressAndHoldEnabled -bool false
+defaults write com.todesktop.230313mzl4w4u92 ApplePressAndHoldEnabled -bool false
