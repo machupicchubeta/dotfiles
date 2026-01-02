26cdf532f51243329fcb8a0accbf26d28d2e6b09
machupicchubeta <machupicchubeta@gmail.com>
2026-01-03 06:30:35 +0900

    Trying out Ruby's ZJIT
    Refs.
    - https://www.ruby-lang.org/en/news/2025/12/25/ruby-4-0-0-released/#zjit
    - https://www.ruby-lang.org/en/news/2025/12/25/ruby-4-0-0-released/#jit
    - https://docs.ruby-lang.org/en/master/jit/zjit_md.html#label-For+normal+use
    - https://railsatscale.com/2025-12-24-launch-zjit/

--- .bash/env_vars.sh
+++ .bash/env_vars.sh
@@ -80,6 +80,8 @@ export GREP_OPTIONS='--color=auto'
 export GUILE_TLS_CERTIFICATE_DIRECTORY="$HOMEBREW_PREFIX/etc/gnutls/"
 
 export PKG_CONFIG_PATH="$HOMEBREW_PREFIX/opt/openssl@3/lib/pkgconfig"
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$HOMEBREW_PREFIX/opt/openssl@3 --with-libyaml-dir=$HOMEBREW_PREFIX/opt/libyaml --with-libffi-dir=$HOMEBREW_PREFIX/opt/libffi --with-readline-dir=$HOMEBREW_PREFIX/opt/readline --with-gmp-dir=$HOMEBREW_PREFIX/opt/gmp  --enable-yjit"
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$HOMEBREW_PREFIX/opt/openssl@3 --with-libyaml-dir=$HOMEBREW_PREFIX/opt/libyaml --with-libffi-dir=$HOMEBREW_PREFIX/opt/libffi --with-readline-dir=$HOMEBREW_PREFIX/opt/readline --with-gmp-dir=$HOMEBREW_PREFIX/opt/gmp  --enable-yjit --enable-zjit"
+# export RUBY_YJIT_ENABLE=1
+export RUBY_ZJIT_ENABLE=1
 
 export GPG_TTY=$(tty)
