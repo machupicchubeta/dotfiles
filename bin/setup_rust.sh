f225ed8c9add8933960fd9f04f7998feb69ae74a
machupicchubeta <machupicchubeta@gmail.com>
2026-07-20 21:30:52 +0900

    Include `rustup` completion in the `zsh-completions` directory where it was installed via Homebrew
    Refs.
    - https://rust-lang.github.io/rustup/installation/index.html#enable-tab-completion-for-bash-fish-zsh-or-powershell
    - https://github.com/zsh-users/zsh-completions
    - https://github.com/machupicchubeta/dotfiles/commit/e88ae89a54d51cd8c8e4200fe365ec4307c2f8ae
    
    Additional notes on `rustup` completion for Bash
    Since the `rustup` completion for Bash was included with `rustup` installed via Homebrew, there was no need to place the files there manually.

--- bin/setup_rust.sh
+++ bin/setup_rust.sh
@@ -6,3 +6,6 @@ mise upgrade rust@latest
 mise use --global rust@latest
 mise prune --yes rust
 mise reshim
+
+: "${HOMEBREW_PREFIX:=/opt/homebrew}"
+if [[ -d "$HOMEBREW_PREFIX/opt/zsh-completions/share/zsh-completions" ]]; then rustup completions zsh > "$HOMEBREW_PREFIX/opt/zsh-completions/share/zsh-completions/_rust"; fi
