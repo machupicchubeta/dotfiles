ba64d9c969e9a202c6752fd41dc0b4efdedf8609
machupicchubeta <machupicchubeta@gmail.com>
2026-07-20 20:54:34 +0900

    Install "Rust" using `mise` instead of `rustup` directly
    Refs.
    - https://mise.en.dev/lang/rust.html
    - https://github.com/jdx/mise/blob/126e7755cc22e36c3d206b650de613951146b5e3/docs/lang/rust.md

--- bin/setup_rust.sh
+++ bin/setup_rust.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 set -eu
 
-: "${XDG_DATA_HOME:=$HOME/.local/share}"
-: "${RUSTUP_HOME:=$XDG_DATA_HOME/rustup}"
-: "${CARGO_HOME:=$XDG_DATA_HOME/cargo}"
-
-if [[ ! -d "$CARGO_HOME/bin" ]]; then RUSTUP_INIT_SKIP_PATH_CHECK=yes rustup-init -y --no-update-default-toolchain --no-modify-path; fi
-rustup default stable
-rustup update
-
-: "${HOMEBREW_PREFIX:=/opt/homebrew}"
-if [[ ! -f "$HOMEBREW_PREFIX/etc/bash_completion.d/rustup.bash-completion" ]]; then rustup completions bash > "$HOMEBREW_PREFIX/etc/bash_completion.d/rustup.bash-completion"; fi
-if [[ ! -d "$RUSTUP_HOME" ]]; then mkdir -p "$RUSTUP_HOME"; fi
-if [[ ! -f "$RUSTUP_HOME/completions" ]]; then rustup completions zsh > "$RUSTUP_HOME/completions"; fi
+mise install rust@latest
+mise upgrade rust@latest
+mise use --global rust@latest
+mise prune --yes rust
+mise reshim
