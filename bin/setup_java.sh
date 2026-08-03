c3cc7a0395b13a60e12707f8c9c5770200473de4
machupicchubeta <machupicchubeta@gmail.com>
2026-08-03 22:49:37 +0900

    Install the latest LTS version of Java, rather than the latest general release version
    After all, it's the latest LTS version that I actually want to use.
    
    - https://www.oracle.com/java/technologies/java-se-support-roadmap.html

--- bin/setup_java.sh
+++ bin/setup_java.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eu
 
-mise install java@latest
-mise upgrade java@latest
-mise use --global java@latest
+mise install java@lts
+mise upgrade java@lts
+mise use --global java@lts
 mise prune --yes java
 mise reshim
 
-sudo ln -sfn "$(mise where java@latest)" "/Library/Java/JavaVirtualMachines/openjdk.jdk"
+sudo ln -sfn "$(mise where java@lts)" "/Library/Java/JavaVirtualMachines/openjdk.jdk"
